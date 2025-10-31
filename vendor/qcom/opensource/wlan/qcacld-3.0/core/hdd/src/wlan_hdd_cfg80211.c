514343cbdc894ab6a222d992eec5e32731babe3b
kimocoder <christian@aircrack-ng.org>
2025-10-31 05:03:58 +0100

    qcacld 3.0: add monitor mode support to netlink
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>
    Signed-off-by: Robin <hanya8493@gmail.com>

--- vendor/qcom/opensource/wlan/qcacld-3.0/core/hdd/src/wlan_hdd_cfg80211.c
+++ vendor/qcom/opensource/wlan/qcacld-3.0/core/hdd/src/wlan_hdd_cfg80211.c
@@ -23561,6 +23561,7 @@ static bool hdd_is_client_mode(enum QDF_OPMODE mode)
 	case QDF_STA_MODE:
 	case QDF_P2P_CLIENT_MODE:
 	case QDF_P2P_DEVICE_MODE:
+	case QDF_MONITOR_MODE:
 		return true;
 	default:
 		return false;
@@ -23572,6 +23573,7 @@ static bool hdd_is_ap_mode(enum QDF_OPMODE mode)
 	switch (mode) {
 	case QDF_SAP_MODE:
 	case QDF_P2P_GO_MODE:
+	case QDF_MONITOR_MODE:
 		return true;
 	default:
 		return false;
